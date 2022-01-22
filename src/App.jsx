cf86eb6b016390548fced7a6e583ccf9ab2ad092
MomoseYohiaki <53268028+MomoseYohiaki@users.noreply.github.com>
2022-01-22 10:23:02 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "25px"
+    // 本来のCSSの書き方ではダメ。キャメルケースで書く。
+    // font-size: "25px"
+  };
+
+  return (
+    // <React.Fragment>
+    //   <h1>こんにちは！</h1>
+    //   <p>お元気ですか？</p>
+    // </React.Fragment>
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
+
+export default App;
