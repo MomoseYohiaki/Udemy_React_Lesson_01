89cc7cff6ff652091b3b464951a5b64917279306
MomoseYohiaki <53268028+MomoseYohiaki@users.noreply.github.com>
2022-01-23 12:06:40 +0900

    useStateの使い方 useStateの使い方を簡単なカウントアップ関数を作成して学んだ

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
-  // const contentStyle = {
-  //   color: "blue",
-  //   fontSize: "25px"
-  //   // 本来のCSSの書き方ではダメ。キャメルケースで書く。
-  //   // font-size: "25px"
-  // };
-  // const contentLedyStyle = {
-  //   color: "pink",
-  //   fontSize: "25px"
-  // };
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const [num, setNum] = useState(0);
 
   return (
-    // <React.Fragment>
-    //   <h1>こんにちは！</h1>
-    //   <p>お元気ですか？</p>
-    // </React.Fragment>
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      {/* <p style={contentStyle}>お元気ですか？</p> */}
-
-      <ColorfulMessage color="blue" message="お元気ですか" />
       <ColorfulMessage color="blue">お元気ですか</ColorfulMessage>
-      {/* <p style={contentLedyStyle}>元気です!</p> */}
-      <ColorfulMessage color="pink" message="元気です" />
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
-
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
